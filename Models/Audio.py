544f42e802111eb4875c43747d65468e51aac111
Manveer Singh <singh.manveer93@icloud.com>
2023-05-06 10:00:02 +0200

    :recycle: Change Audio constructor

--- Models/Audio.py
+++ Models/Audio.py
@@ -10,24 +10,39 @@ class Audio:
         path
     """
 
-    def __init__(self):
-        self.name = None
-        self.data = None
-        self.encoding = None
-        self.path = None
+    def __init__(self, json_dic=None):
+        if json_dic:
+            if json_dic["data"]:
+                self.set_data(base64.b64decode(json_dic["data"]))
+            if json_dic["name"]:
+                self.set_name(json_dic["name"])
+            if json_dic["path"]:
+                self.set_path(json_dic["path"])
+            self.change_data_format_from_b64_into_b2()
+        else:
+            self.name = None
+            self.data = None
+            self.encoding = None
+            self.path = None
 
-    def __int__(self, name, data, encoding, path):
-        self.name = name
+    def set_data(self, data):
         self.data = data
-        self.encoding = encoding
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_path(self, path):
         self.path = path
+
+    def set_encoding(self, encoding):
+        self.encoding = encoding
 
     def change_data_format_from_b64_into_b2(self):
         if self.encoding == "b64" or self.encoding is None:
-            self.data = base64.b64decode(self.data)
-            self.encoding = "b2"
+            self.set_data(base64.b64decode(self.data))
+            self.set_encoding("b2")
 
     def change_data_format_from_b2_into_b64(self):
         if self.encoding == "b2" or self.encoding is None:
-            self.data = base64.b64encode(self.data)
-            self.encoding = "b64"
+            self.set_data(base64.b64encode(self.data))
+            self.set_encoding("b64")
--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 import base64
 from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.middleware.cors import CORSMiddleware  # LOCAL USE ONLY
-from Controllers import audioController as audioCtrl
+from Controllers import audioController
+from Models.Audio import Audio
 
 app = FastAPI()
 
@@ -27,11 +28,8 @@ app.add_middleware(
 
 @app.post("/audio")
 async def read_item(base64_audio: Request):
-    # Récupère le son en json
-    json_data = await base64_audio.json()
-    # Décode le son base64 en donées binaire
-    audio_bytes = base64.b64decode(json_data["data"])
-    print(audio_bytes)
+    audio = Audio(await base64_audio.json())
+    print(audio.data)
 
 
 @app.post('/upload')
