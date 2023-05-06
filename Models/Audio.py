71a986f70c92ce1f04ce8b27e310c3d993c4700b
Manveer Singh <singh.manveer93@icloud.com>
2023-05-06 10:15:36 +0200

    :recycle: Fix Audio constructor

--- Models/Audio.py
+++ Models/Audio.py
@@ -4,38 +4,37 @@ import base64
 class Audio:
     """
     Properties:
-        name
         data
         encoding
         path
+        extension
     """
 
     def __init__(self, json_dic=None):
+        self.data = None
+        self.encoding = None
+        self.path = None
+        self.extension = None
         if json_dic:
             if json_dic["data"]:
-                self.set_data(base64.b64decode(json_dic["data"]))
-            if json_dic["name"]:
-                self.set_name(json_dic["name"])
+                self.set_data(json_dic["data"])
             if json_dic["path"]:
                 self.set_path(json_dic["path"])
+            if json_dic["extension"]:
+                self.set_extension(json_dic["extension"])
             self.change_data_format_from_b64_into_b2()
-        else:
-            self.name = None
-            self.data = None
-            self.encoding = None
-            self.path = None
 
     def set_data(self, data):
         self.data = data
-
-    def set_name(self, name):
-        self.name = name
 
     def set_path(self, path):
         self.path = path
 
     def set_encoding(self, encoding):
         self.encoding = encoding
+
+    def set_extension(self, extension):
+        self.extension = extension
 
     def change_data_format_from_b64_into_b2(self):
         if self.encoding == "b64" or self.encoding is None:
